fd1e8cb9c5447e05624585f516ed623253e4f90d
Oleksandr Perekuta <perekuta.a@gmail.com>
2023-10-13 18:21:29 +0200

    Initial commit

---
+++ uploadapp/src/main/java/com/uploadapp/UploadappApplication.java
@@ -0,0 +1,13 @@
+package com.uploadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadappApplication.class, args);
+	}
+
+}
